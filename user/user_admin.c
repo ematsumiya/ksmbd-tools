4d57e0184f35079b7075dd2fd48403becb9136d8
Enzo Matsumiya <ematsumiya@suse.de>
2022-01-05 17:59:58 -0300

    ksmbd-tools: use quotes for local includes
    Cosmetic only, but better practice.
    
    Signed-off-by: Enzo Matsumiya <ematsumiya@suse.de>

--- user/user_admin.c
+++ user/user_admin.c
@@ -14,15 +14,13 @@
 #include <fcntl.h>
 #include <termios.h>
 
-#include <config_parser.h>
-#include <ksmbdtools.h>
-
-#include <md4_hash.h>
-#include <user_admin.h>
-#include <management/user.h>
-#include <management/share.h>
-
-#include <linux/ksmbd_server.h>
+#include "config_parser.h"
+#include "ksmbdtools.h"
+#include "md4_hash.h"
+#include "user_admin.h"
+#include "management/user.h"
+#include "management/share.h"
+#include "linux/ksmbd_server.h"
 
 #define MAX_NT_PWD_LEN 129
 
