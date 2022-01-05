fee65cf4ab108ec8e4631a2c8847dd913950dcc0
Enzo Matsumiya <ematsumiya@suse.de>
2022-01-05 18:41:26 -0300

    share: introduce share_cmd
    Create share command in preparation for binary unification.
    
    Introduce a command to list available shares.
    
    Makes open_smbconf() optionally truncate the file (for list command).
    
    Signed-off-by: Enzo Matsumiya <ematsumiya@suse.de>

--- share/share_admin.h
+++ share/share_admin.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
  *   Copyright (C) 2019 Samsung Electronics Co., Ltd.
+ *   Copyright (C) 2021 SUSE LLC
  *
  *   linux-cifsd-devel@lists.sourceforge.net
  */
@@ -8,8 +9,36 @@
 #ifndef __KSMBD_SHARE_ADMIN_H__
 #define __KSMBD_SHARE_ADMIN_H__
 
-int command_add_share(char *smbconf, char *name, char *opts);
-int command_update_share(char *smbconf, char *name, char *opts);
-int command_del_share(char *smbconf, char *name);
+int share_add_cmd(char *smbconf, char *name, char *opts);
+int share_delete_cmd(char *smbconf, char *name);
+int share_update_cmd(char *smbconf, char *name, char *opts);
+int share_list_cmd(char *smbconf);
+
+typedef enum {
+	KSMBD_CMD_SHARE_NONE = 0,
+	KSMBD_CMD_SHARE_ADD,
+	KSMBD_CMD_SHARE_DELETE,
+	KSMBD_CMD_SHARE_UPDATE,
+	KSMBD_CMD_SHARE_LIST,
+	KSMBD_CMD_SHARE_MAX
+} ksmbd_share_cmd;
+
+/* List of supported subcommands */
+static const char *ksmbd_share_cmds_str[] = {
+	"none",
+	"add",
+	"delete",
+	"update",
+	"list"
+};
+
+static struct option share_opts[] = {
+        { "conf", required_argument, NULL, 'c' },
+        { "options", required_argument, NULL, 'o' },
+	{ 0, 0, 0, 0 },
+};
+
+void share_usage(ksmbd_share_cmd cmd);
+int share_cmd(int argc, char *argv[]);
 
 #endif /* __KSMBD_SHARE_ADMIN_H__ */
